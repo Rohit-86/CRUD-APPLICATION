97a2717f2fbf6277bf67113717dd6798128167ce
Rohit <singh748830@gmail.com>
2024-01-30 00:34:31 +0530

    curd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import AllUsers from "./components/AllUsers";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/all" element={<AllUsers />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
